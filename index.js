25ac5e2fa088840bf48e77a9806c8b6a8115d8b2
dialloflatno <brahimdiallo23@gmail.com>
2021-10-17 04:08:11 -0400

    commit tree d71df1bdd3d866e8741e1f884f3806eb31e0ae18

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = ' name '
+const upperCaseCurrentUser = currentUser.toUpperCase
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice;
+const welcomeMessage = `Welcome to Flatbook,${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${restOfName}!`
